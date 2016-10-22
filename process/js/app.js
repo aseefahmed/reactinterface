6969fc0a768c8bb02acec94f994a2aed9d7723dc
aseef ahmed <aseef@localhost.localdomain>
2016-10-22 22:25:18 +0600

    commit

---
+++ process/js/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var MainInterface = React.createClass({
+	render: function(){
+		return <h1>Aseef Ahmed TEST</h1>
+	}
+});
+
+ReactDOM.render(<MainInterface />, document.getElementById('petAppointments'));
